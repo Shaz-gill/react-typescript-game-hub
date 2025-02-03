43ac8ecc4a672ccb6c9eac6dceaffcf1477627bf
Shaz Gill <shazgill@Shazs-Air.lan>
2025-02-03 23:01:59 +0800

    Displaying the genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,9 +12,9 @@ const GameGrid = () => {
     <>
       {error && <p>{error}</p>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding="10px"
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
