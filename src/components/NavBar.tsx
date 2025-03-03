fd169994ce07bc0b30ca0f86300f60171d2e4599
Shaz Gill <shazgill@Mac.lan>
2025-03-03 18:09:52 +0800

    Removed unnecessary code from NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,10 @@
-import { HStack, Image, useColorMode } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { useLogo } from "../hooks/useThemeStyles";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
-  const { colorMode } = useColorMode();
-  const logo = colorMode;
-
   return (
     <HStack px={5} py={7}>
       <Link to="/">
